5fe638e10e52ad596801bcf001d9a5820064f8c7
atique.atq <atique.atq@gmail.com>
2021-10-20 16:29:29 +0600

    added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 import Home from './Pages/Home/Home/Home';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }

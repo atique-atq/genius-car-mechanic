73b0dc556dd1467022f19e7c31e780dcbf37e016
atique.atq <atique.atq@gmail.com>
2021-10-21 21:35:11 +0600

    added firebase config

---
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCwNGEDC344EgMC84p9vCHH2cGKJFysh_k",
+    authDomain: "genius-car-mechanic-b1797.firebaseapp.com",
+    projectId: "genius-car-mechanic-b1797",
+    storageBucket: "genius-car-mechanic-b1797.appspot.com",
+    messagingSenderId: "707390233504",
+    appId: "1:707390233504:web:b9a99ae488f582377c62c5"
+};
+
+export default firebaseConfig;
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <>
-            <Navbar bg="dark" variant="dark" sticky="top">
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top">
                 <Container>
                     <Navbar.Brand href="#home">Genius Car Mechanic</Navbar.Brand>
                     <Navbar.Toggle />
                     <Navbar.Collapse className="justify-content-end">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#services">Services </Nav.Link>
-                        <Nav.Link href="#experts">Experts</Nav.Link>
+                        <Nav.Link as={Link} to="/home#home">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/home#services">Services </Nav.Link>
+                        <Nav.Link as={Link} to="/home#experts">Experts</Nav.Link>
+                        <Nav.Link as={Link} to="/login">Login</Nav.Link>
                         <Navbar.Text>
                             Signed in as: <a href="#login">Mark Otto</a>
                         </Navbar.Text>
